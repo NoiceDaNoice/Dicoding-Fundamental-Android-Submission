97358eeb2c331ad7213961f81325f244e4ba8b2d
Gary Adam <jioe656@gmail.com>
2021-06-03 18:12:20 +0700

    submission 3

--- app/src/main/java/com/example/githubapp3/api/iUser.java
+++ app/src/main/java/com/example/githubapp3/api/iUser.java
@@ -16,31 +16,31 @@ import retrofit2.http.Query;
 
 public interface iUser {
     @GET("/search/users")
-    @Headers("Authorization: token b6c1769cbfa65838ce23e3c7821176bde327d9d1")
+    @Headers("Authorization: token ***************")
     Call<JSONRespone> getSearch(
             @Query("q") String username
     );
 
     @GET("/users/{username}")
-    @Headers("Authorization: token b6c1769cbfa65838ce23e3c7821176bde327d9d1")
+    @Headers("Authorization: token ***************")
     Call<DetailUserGithubModel> getDetail(
             @Path("username") String username
     );
 
     @GET("/users/{username}")
-    @Headers("Authorization: token b6c1769cbfa65838ce23e3c7821176bde327d9d1")
+    @Headers("Authorization: token ***************")
     Call<DetailFavUserGithubModel> getDetailFav(
             @Path("username") String username
     );
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token b6c1769cbfa65838ce23e3c7821176bde327d9d1")
+    @Headers("Authorization: token ***************")
     Call<List<FollowerModel>> getFollower(
             @Path("username") String username
     );
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token b6c1769cbfa65838ce23e3c7821176bde327d9d1")
+    @Headers("Authorization: token ***************")
     Call<List<FollowingModel>> getFollowing(
             @Path("username") String username
     );
